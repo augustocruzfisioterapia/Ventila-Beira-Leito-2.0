0cd507da0b9d64b13aefac01740c7c4e52db636a
Augusto Cruz <137422882+augustocruzfisioterapia@users.noreply.github.com>
2026-06-06 16:28:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Ventila Beira-Leito - Progressive Web App
 // Versão: v13 (atualize este número sempre que modificar o app)
 
-const CACHE_NAME = "ventila-beira-leito-v16";
+const CACHE_NAME = "ventila-beira-leito-v17";
 
 // Arquivos essenciais para o funcionamento offline do app
 const FILES_TO_CACHE = [
